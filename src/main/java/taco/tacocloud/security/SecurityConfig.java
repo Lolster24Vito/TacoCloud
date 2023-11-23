10e6993163e7c3a793ba59f1c5857786f79a45b0
Lolster24Vito <vitomir.hardi@gmail.com>
2023-11-23 02:18:56 +0100

    Chapter 5.3 login works

--- src/main/java/taco/tacocloud/security/SecurityConfig.java
+++ src/main/java/taco/tacocloud/security/SecurityConfig.java
@@ -1,23 +1,55 @@
 package taco.tacocloud.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.io.IOException;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig  {
+    //public PasswordEncoder passwordEncoder(){
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    private UserDetailsService userService;
+
+    @Autowired
+    public void setUserService(UserDetailsService userService) {
+        this.userService = userService;
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+//unneded old days of extends WebSecurityConfigurerAdapter {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
     }
     //antMatchers have been deprecated and removed with Spring Security 6.0
 
@@ -42,18 +74,31 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
 
         HttpSecurity httpSecurity = http
+                .csrf(c->c.disable()).headers(h->h.frameOptions(f->f.disable()).disable())
 
 
                 .authorizeHttpRequests((authorize) -> authorize
+                                .requestMatchers(antMatcher("/h2-console/**")).anonymous()
                         .requestMatchers(antMatcher("/design")).hasRole("USER")
                         .requestMatchers(antMatcher("/orders")).hasRole("USER")
-                        .requestMatchers(antMatcher("/login")).permitAll()
-                        .requestMatchers(mvc.pattern("/**")).permitAll()
+                                .anyRequest().permitAll()
+                      //  .requestMatchers(mvc.pattern("/**")).permitAll()
 
                 )
                 .formLogin(formLogin -> formLogin
-                        .loginPage("/login").defaultSuccessUrl("/design",true)
-                        .permitAll()
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/design",true)
+                        .failureUrl("/login.html?error=true").successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                System.out.println("Logged user: " + authentication.getName());
+
+                                response.sendRedirect("/");
+                            }
+                        })
                 )
 
                 ;
--- src/main/java/taco/tacocloud/security/UserDetailsManagerImpl.java
+++ src/main/java/taco/tacocloud/security/UserDetailsManagerImpl.java
@@ -2,22 +2,30 @@ package taco.tacocloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import taco.tacocloud.TacoUser;
 import taco.tacocloud.data.UserRepository;
 
 @Configuration
-public class UserDetailsManagerImpl {
+public class UserDetailsManagerImpl implements UserDetailsService{
     private UserRepository userRepo;
     public UserDetailsManagerImpl(UserRepository userRepo){
         this.userRepo=userRepo;
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        TacoUser user = userRepo.findByUsername(username);
+        if (user != null) {
+            return user;
+        }
+        throw new UsernameNotFoundException(                "User '" + username + "' not found");
+    }
     /*
 
     @Bean public UserDetailsService userDetailsService(UserRepository userRepo){
-
-     */
     @Bean public UserDetailsService userDetailsService(){
         return username -> {
           TacoUser tacoUser =userRepo.findByUsername(username);
@@ -25,4 +33,6 @@ public class UserDetailsManagerImpl {
           throw new UsernameNotFoundException("User: "+username+" not found");
         };
     }
+     */
+
 }
